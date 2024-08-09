81b7d6f3d54a40de835de3dbdfe3c2914e87ae00
janvi <laxmijaswal189@gmail.com>
2024-08-09 04:32:52 -0400

    add delivery information table

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -63,7 +63,7 @@ const Cart = () => {
             <p>If you have a promo code, Enter it here</p>
             <div className="cart-promocode-input">
               <input type="text" placeholder='promo code' />
-              
+              <button>Submit</button>
             </div>
           </div>
         </div>
